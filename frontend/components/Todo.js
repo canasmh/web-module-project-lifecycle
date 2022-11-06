e8d08b30d69a43e4c237321f23c869195fd1ba91
canasmh <canasmh@yahoo.com>
2022-11-06 16:32:27 -0500

    Put check mark by todo item if item is completed

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -3,6 +3,8 @@ import React from 'react'
 export default class Todo extends React.Component {
 
   render() {
-    return (<p onClick={() => this.props.patch(this.props.id)}>{this.props.name}</p>)
+    return (
+      <p className="todo" onClick={() => this.props.patch(this.props.id)}>{this.props.name}{this.props.completed && <span>✅</span>}</p>
+    )
   }
 }
