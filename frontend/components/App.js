0f450a5cb08e31b223475aedfc1146442ceb043a
canasmh <canasmh@yahoo.com>
2022-11-06 15:38:51 -0500

    Use CDM to make an axios get request and display that data to the UI

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(URL)
+      .then(response => {
+        const data = response.data.data;
+        this.setState({todos: data})
+      })
+      .catch(err => {
+        console.log(`There was an error: ${err}`)
+      })
+  }
   render() {
-    return null
+    return (
+      <>
+        {this.state.todos && this.state.todos.map(item => <p>{item.name}</p>)}
+      </>
+    )
   }
 }
